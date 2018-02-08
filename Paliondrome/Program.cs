208709b93bc58c164aad0bb9d44a6359e0de51df
iyosiyas wolde <iyosiyasbeyene@yahoo.com>
2018-02-08 14:37:11 -0800

    palindrome

--- Paliondrome/Program.cs
+++ Paliondrome/Program.cs
@@ -6,7 +6,23 @@ namespace Paliondrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			string a = string.Empty;
+            Console.WriteLine("Enter string");
+			a = Console.ReadLine();
+
+			Char[] Temp = a.ToCharArray();
+			Array.Reverse(Temp);
+			string b = new string(Temp);
+
+			if (a.ToLower().Equals(b.ToLower()))
+			{
+				Console.WriteLine("Palindrome");
+			}
+			else
+			{
+				Console.WriteLine(" Not a Palindrome");
+			}
+			Console.ReadLine();
         }
     }
 }
